270c826524f3252927f0d47e464c62f7f4dc0927
Sinan Parmar <sinanparmar2@gmail.com>
2020-06-24 14:58:34 +0300

    Update faker_boun.py

--- faker_boun.py
+++ faker_boun.py
@@ -1,30 +1,35 @@
 import numpy as np
 import pandas as pd
-import noise
+from noise import pnoise1
 
 
 
 #This is a python module to create fake seismological data
 
-def noisef(time, noise_type='gaussian', std=1, mean=0, outtype ='ndarray', noise_octave = False):
+def generate_noise(timeLength, noise_type='gaussian', samplingRate = 20 ,outputType ='ndarray', noise_octave = False, t_start = 0):
 	
-	return np.random.normal(mean,std,time)
+	
 	if noise_type == 'perlin':
 		
-
-		t = np.linspace(0,time,20*100)
+		t = np.linspace(0,timeLength,timeLength*samplingRate)
 		y1 = np.array(list(map(lambda y: pnoise1(y), t)))
 
-		noise = np.random.normal(mean,std,time)
-		y2 = np.add(y1,noise.pnoise1)
+		noise = np.random.normal(size = samplingRate*timeLength)
+		y2 = np.add(y1, noise)
 
 		return y2
 
-	#if noise_type == 'simplex' Not now
+	else:
+		return np.random.normal(size=timeLength*samplingRate)
 
 
 
-def noisef_and_signal(time, noise_type='gaussian', std=1,mean=0,outtype='ndarray',signal = 'seismic'):
-	import noise
+def generate_eq(timeLength, eqCount=1,  samplingRate = 20 ,outputType ='ndarray', t_start = 0):
 	return
+
+
+
+def generate_noise_and_eq(timeLength, noise_type='gaussian', eqCount=1, samplingRate = 20 ,outputType ='ndarray', noise_octave = False, t_start = 0):
+
+	return generate_eq(timeLength, eqCount, samplingRate, outputType, t_start) + generate_noise(timeLength, noise_type, samplingRate, outputType,noise_octave, t_start)
 
