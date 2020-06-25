e3359d2779b04b968209479b646d59ec4cb64b40
CemNurlu <cemnurlu@gmail.com>
2020-06-25 22:27:13 +0300

    Create seisgen.py

---
+++ seisgen.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random as rd
+
+def some_f(sigma, fn, zeta, f, T90, eps, tn):
+    
+    #Init
+    w = 2 * np.pi * f
+    fs = f[-1]
+    dt = 1 / fs
+    f0 = np.median(np.diff(f))
+    Nfreq = len(f)
+    t = np.arange(0,(Nfreq-1)*dt+1,dt)
+    
+    #Generation of spectrum S
+    
+    fn = fn * 2 * np.pi #transformation in rad
+    s0 = 2 * zeta * (sigma**2) / (np.pi * fn * (4 * (zeta**2) + 1))
+    A = (fn**4) + (2*zeta*fn*w)**2
+    B = ((fn**2 -  w**2)**2) + (2*zeta*fn*w)**2
+    S = s0 * A / B #Single sided PSD
+    
+    #Time series generation - Monte Carlo simulation
+    
+    A = np.sqrt(2*S*f0)
+    B = w.T * t + 2*np.pi * np.kron(np.random.rand(Nfreq,), np.arange(1,Nfreq))
+    x = A * B #stationary process
+    
+    #Envelop function E
+    b = -eps * np.log(T90) / (1 + eps * (np.log(T90) - 1))
+    c = b / eps
+    a = (np.exp(1) / eps)**b
+    E = a * (t /tn)**b * np.exp(-c*t/tn)
+    
+    #Envelop multiplied with stationary process to get y
+    
+    y = x * E
+    
+    return(y,t)
