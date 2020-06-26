7aea74e0323b198b5d8df009be67fc28c2c794b0
Sinan Parmar <sinanparmar2@gmail.com>
2020-06-26 09:59:14 +0300

    implemented seisgen into faker_boun

--- example1.py
+++ example1.py
@@ -1,7 +1,7 @@
 import faker_boun as fk 
 import matplotlib.pyplot as plt
 
-c = fk.generate_noise_and_eq(100)
+c = fk.generate_noise_and_eq(100, scale = 100, noise_type='perlin')
 
 
 #plt.figure(figsize=(40,20))
