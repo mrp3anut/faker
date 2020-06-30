6502dee29fee280c2f9e38b866e232579da7a753
CemNurlu <cemnurlu@gmail.com>
2020-07-01 01:23:34 +0300

    Revert "Removed redundant scaling"
    This reverts commit cf56e54fbbed2bd1dec816c03131ea0ff0b1588d.

--- faker_boun.py
+++ faker_boun.py
@@ -4,10 +4,10 @@ from noise import pnoise1
 #This is a python module to create fake seismological data
 
 def generate_noise(timeLength, noise_type='gaussian', samplingRate = 100 ,outputType ='ndarray', noise_octave = False, t_start = 0):
-
-
+	
+	
 	if noise_type == 'perlin':
-
+		
 		t = np.linspace(0,timeLength,timeLength*samplingRate)
 		y1 = np.array(list(map(lambda y: pnoise1(y), t)))
 
@@ -19,12 +19,12 @@ def generate_noise(timeLength, noise_type='gaussian', samplingRate = 100 ,output
 	else:
 		return np.random.normal(size=timeLength*samplingRate)
 
-
+	
 
 def generate_eq(timeLength, eqCount=1, samplingRate = 100 ,outputType ='ndarray', t_start = 0, scale=10, params = {"zeta" : 0.3, "sigma" : 0.9,"fn" :5,"T90" : 0.3,"eps" : 0.4,"tn" : 30}):
+    
 
-
-    zeta, sigma, fn, T90, eps, tn = params['zeta'], params['sigma'], params['fn'], params['T90'], params['eps'], params['tn']
+    zeta, sigma, fn, T90, eps, tn = params['zeta'], params['sigma'], params['fn'], params['T90'], params['eps'], params['tn'] 
 
     #Init
     f = np.linspace(t_start,40,timeLength*samplingRate)
@@ -34,33 +34,33 @@ def generate_eq(timeLength, eqCount=1, samplingRate = 100 ,outputType ='ndarray'
     f0 = np.median(np.diff(f))
     Nfreq = len(f)
     t = np.arange(0,dt*(Nfreq),dt)
-
+    
     #Generation of spectrum S
-
+    
     fn = fn * 2 * np.pi #transformation in rad
     s0 = 2 * zeta * (sigma**2) / (np.pi * fn * (4 * (zeta**2) + 1))
     A = (fn**4) + (2*zeta*fn*w)**2
     B = ((fn**2 -  w**2)**2) + (2*zeta*fn*w)**2
     S = s0 * A / B #Single sided PSD
-
+    
     #Time series generation - Monte Carlo simulation
-
+    
     A = np.sqrt(2 * S * f0)
     B = np.cos((w.T * t )+ 2*np.pi * np.tile(np.random.randint(0,Nfreq), (1,Nfreq)))
     #print(np.shape(B))
     x = A * B #stationary process
-
+    
     #Envelop function E
     b = -eps * np.log(T90) / (1 + eps * (np.log(T90) - 1))
     c = b / eps
     a = (np.exp(1) / eps)**b
     E = a * (t /tn)**b * np.exp(-c*t/tn)
-
+    
     #Envelop multiplied with stationary process to get y
-
+    
     y = x * E
-
-    return y[0]*scale
+    
+    return y[0]*scale*100
 
 
 
